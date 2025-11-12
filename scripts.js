e081d9383bf93cde9e178054cc22ac2e55f9e143
Godsminion777 <hriettie@ymail.com>
2025-11-12 15:38:03 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
+<<<<<<< HEAD
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // Select the form, the email input field, and the message div
+    const newsletterForm = document.querySelector('.newsletter form');
+    const emailInput = document.getElementById('email');
+    const messageDiv = document.querySelector('.message');
+
+    
+    newsletterForm.addEventListener('submit', function(event) {
+        // Prevent the form's default submission which reloads the page
+        event.preventDefault();
+
+        // Get the value from the email input field and trim whitespace
+        const emailValue = emailInput.value.trim();
+
+        // Check if the email field is empty
+        if (emailValue === '') {
+            // Display the error message if no email is entered
+            messageDiv.textContent = 'Please enter a valid email address.';
+        } else {
+           
+            messageDiv.innerHTML = 'Thank you! Your email address <strong>' + emailValue + '</strong> has been added to our mailing list!';
+            
+            // Clears the input field after successful submission
+            emailInput.value = '';
+        }
+    });
+});
+=======
+/* Add your JavaScript to this file */
+>>>>>>> 523d8fd63b68f64377e732c26c02c51ecc04c3dd
